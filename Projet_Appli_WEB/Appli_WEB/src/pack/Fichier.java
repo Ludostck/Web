4dad70a20eb65cb9f21b7fc96dcc08eefa38732b
barbaraDoCoutoVidal <120388312+barbaraDoCoutoVidal@users.noreply.github.com>
2024-05-16 09:29:53 +0200

    Add files via upload

--- Projet_Appli_WEB/Appli_WEB/src/pack/Fichier.java
+++ Projet_Appli_WEB/Appli_WEB/src/pack/Fichier.java
@@ -1,10 +1,13 @@
 package pack;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
+
+@Entity
 public class Fichier {
 	
 	@Id
