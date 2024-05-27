c0dbb5e6b20ccd0f4b6418b06188205e6d9365f2
barbaraDoCoutoVidal <120388312+barbaraDoCoutoVidal@users.noreply.github.com>
2024-05-27 13:57:46 +0200

    Add files via upload

---
+++ Projet.java
@@ -0,0 +1,62 @@
+package pack;
+
+import javax.persistence.*;
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+@Entity
+public class Projet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @ManyToOne
+    @JsonBackReference
+    private Session owner;
+
+
+    @OneToMany(mappedBy = "projet", fetch = FetchType.EAGER)
+    private List<Dossier> dossiers;
+    
+	@ManyToMany(mappedBy = "projets", fetch = FetchType.EAGER)
+	private List<MotCle> motscles;
+	
+
+    // getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Session getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Session owner) {
+        this.owner = owner;
+    }
+
+    public List<Dossier> getDossiers() {
+        return dossiers;
+    }
+
+    public void setDossiers(List<Dossier> dossiers) {
+        this.dossiers = dossiers;
+    }
+}
